9ebf21e4f6b9b3f76bfe13aeec71fdf793cc3a8a
hnthinh2909 <hnthinh2909@gmail.com>
2020-06-13 18:12:29 +0700

    Add edit avatar and info in profile

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -1,13 +1,53 @@
 const db = require("../db.js");
 const shortid = require('shortid');
 
+var avatarUrl = "";
+
+var cloudinary = require('cloudinary').v2;
+
+cloudinary.config({ 
+  cloud_name: process.env.UPLOAD_CLOUD_NAME,
+  api_key: process.env.UPLOAD_API_KEY, 
+  api_secret: process.env.UPLOAD_API_SECRET
+});
+
+
 module.exports.index = function(req, res, next) {
 	let user = db.get("users").find({id:req.signedCookies.userId}).value();
 	let avatar = user.avatar;
 	let name = user.name;
 	let email = user.email;
-	console.log(name, email);
+	let phone = user.number;
 	res.render("profile/index", {
-		name, email, avatar
+		name, email, avatar, phone
 	});
+}
+
+module.exports.editInfo = function(req, res, next) {
+	res.render("profile/edit-info");
+}
+
+module.exports.editInfoPost = function(req, res, next) {
+	db.get('users')
+	  .find({id: req.signedCookies.userId})
+	  .assign({ name: req.body.name, number: req.body.number})
+	  .write();
+	res.redirect("/profile");
+}
+
+module.exports.editAvatar = function(req, res, next) {
+	res.render("profile/edit-avatar");
+}
+
+module.exports.editAvatarPost = function(req, res, next) {
+	const file = req.file.path;
+	cloudinary.uploader.upload(req.file.path, function(error, result) { 
+		req.body.avatar= result.url; 
+		db.get("users")
+		.find({id: req.signedCookies.userId})
+		.assign({avatar: req.body.avatar})
+		.write();
+	}); 
+	// req.body.avatar = avatarUrl.url;
+	res.redirect("/profile");
 }
--- routes/profile-route.js
+++ routes/profile-route.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const multer  = require('multer');
+const upload = multer({ dest: './public/uploads/' });
+
 const profileController = require("../controllers/profile-controller.js");
 
 router.get("/", profileController.index);
 
+router.get("/editinfomation", profileController.editInfo);
+
+router.post("/editinfomation", profileController.editInfoPost);
+
+router.get("/editavatar", profileController.editAvatar);
+
+router.post("/editavatar", upload.single('avatar'), profileController.editAvatarPost);
 
 module.exports = router;
