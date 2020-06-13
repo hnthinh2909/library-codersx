6f011736fe5abab488de023c62929882ee3b9372
hnthinh2909 <hnthinh2909@gmail.com>
2020-06-13 14:24:04 +0700

    Update img profile use source avatar

--- controllers/profile-controller.js
+++ controllers/profile-controller.js
@@ -3,10 +3,11 @@ const shortid = require('shortid');
 
 module.exports.index = function(req, res, next) {
 	let user = db.get("users").find({id:req.signedCookies.userId}).value();
+	let avatar = user.avatar;
 	let name = user.name;
 	let email = user.email;
 	console.log(name, email);
 	res.render("profile/index", {
-		name, email
+		name, email, avatar
 	});
 }
